f25ab014834d0e8782eb6aabc1e1b1025a294583
Tyler <tscizzle@gmail.com>
2016-12-13 20:37:59 -0500

    added fallback braid url in case config doesnt work in some env

--- app/api.js
+++ app/api.js
@@ -1,8 +1,10 @@
 import Config from 'react-native-config';
 
 
+const BRAID_SERVER_URL = Config ? Config.BRAID_SERVER_URL : 'https://preetybraid-staging.herokuapp.com';
+
 export const braidFetch = (...args) => {
-  args[0] = Config.BRAID_SERVER_URL + args[0];
+  args[0] = BRAID_SERVER_URL + args[0];
   return fetch(...args);
 };
 
